a59a7f6cbbb648ae28c944f8575b7df97a16f3e1
aymen <aymen.bua@hotmail.com>
2024-12-15 23:40:27 +0100

    ioc

---
+++ src/main/java/org/sid/ioc_app/IoCAppApplication.java
@@ -0,0 +1,13 @@
+package org.sid.ioc_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoCAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoCAppApplication.class, args);
+    }
+
+}
